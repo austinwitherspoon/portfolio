c38f4cf8b2c35af6bb93e937cfae8ff454de9af7
Austin Witherspoon <austinwitherspoon@users.noreply.github.com>
2023-08-18 17:56:12 -0700

    Initial commit

---
+++ content/programming/love_your_coworkers_by_type_hinting.md
@@ -0,0 +1,10 @@
+Title: Love Your Coworkers By Type-Hinting
+Date: 2023-08-18
+Modified: 2023-08-18
+Category: Programming
+Tags: python, programming, type-hints
+Slug: love-your-coworkers-by-type-hinting
+Authors: Austin Witherspoon
+Summary: In languages like Python, type-hints are an extraordinarily useful tool for communicating and learning your codebase.
+
+to be written
---
+++ pelicanconf.py
@@ -0,0 +1,69 @@
+AUTHOR = "Austin Witherspoon"
+SITENAME = "Austin Witherspoon | Portfolio"
+SITEURL = "http://austinwitherspoon.com"
+SITETITLE = "Austin Witherspoon"
+SITESUBTITLE = "Filmmaker | Software Developer"
+
+# SITELOGO = SITEURL + "/images/profile.png"
+FAVICON = "static/favicon.ico"
+
+BROWSER_COLOR = "#333"
+PATH = "content"
+
+TIMEZONE = "America/Los_Angeles"
+
+DEFAULT_LANG = "English"
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+AUTHOR_FEED_ATOM = None
+AUTHOR_FEED_RSS = None
+
+# Blogroll
+LINKS = (
+    ("Home", "/"),
+    ("About", "/pages/about.html"),
+    (
+        "my youtube channel",
+        "https://www.youtube.com/channel/UCzHlz1lU53PK531OWRVwatQ",
+    ),
+)
+
+# Social widget
+SOCIAL = (
+    ("Twitter", "https://twitter.com/ajwitherspoon"),
+    # ('Another social link', '#'),
+)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
+# RELATIVE_URLS = True
+
+THEME = "themes/Flex"
+
+
+STATIC_PATHS = ["static"]
+CUSTOM_CSS = "static/custom.css"
+
+MAIN_MENU = True
+MENUITEMS = (
+    ("Archives", "/archives.html"),
+    ("Categories", "/categories.html"),
+    ("Tags", "/tags.html"),
+)
+
+# ADD_THIS_ID = "ra-77hh6723hhjd"
+# DISQUS_SITENAME = "yoursite"
+# GOOGLE_ANALYTICS = "UA-1234-5678"
+# GOOGLE_TAG_MANAGER = "GTM-ABCDEF"
+THEME_COLOR = "dark"
+# THEME_COLOR_AUTO_DETECT_BROWSER_PREFERENCE = True
+# THEME_COLOR_ENABLE_USER_OVERRIDE = True
+
+PYGMENTS_STYLE = "emacs"
+PYGMENTS_STYLE_DARK = "monokai"
+
+COPYRIGHT_YEAR = "2023 Austin Witherspoon"
